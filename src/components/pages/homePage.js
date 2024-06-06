ab502f7baa8244e7ddaaefdaf3113cdb4944d670
Wsnlcdmsk <134656590+Wsnlcdmsk@users.noreply.github.com>
2024-06-06 03:25:07 +0300

    some fix

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,57 +1,75 @@
-import React, { useState, useCallback } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { createTheme, Paper } from '@mui/material';
-import API_URL from '../../config';
+import React from 'react';
+import styled from 'styled-components';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-  },
-});
 
 const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
   padding: 2rem;
-  background-color: ${theme.palette.background.paper};
+  font-family: 'Roboto', sans-serif;
+`;
+
+const Header = styled.header`
+  text-align: center;
+  margin-bottom: 3rem;
+
+  h1 {
+    font-size: 3rem;
+    font-weight: bold;
+    color: #333;
+  }
+`;
+
+const HeroSection = styled.section`
+  background-color: #f5f5f5;
+  padding: 4rem 2rem;
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  margin-bottom: 3rem;
 
-const Title = styled.h2`
-  margin-bottom: 1.5rem;
-  color: ${theme.palette.text.primary};
-`;
-
-const InputGroup = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 1rem;
-
-  input {
-    flex: 1;
-    padding: 0.5rem 1rem;
-    border: 1px solid ${theme.palette.grey[400]};
-    border-radius: 4px;
-    font-size: 0.9rem;
-    color: ${theme.palette.text.primary};
+  h2 {
+    font-size: 2.5rem;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 1.5rem;
   }
 
-  button {
-    margin-left: 0.5rem;
-    padding: 0.5rem 1rem;
-    background-color: ${theme.palette.primary.main};
-    color: ${theme.palette.common.white};
-    border: none;
+  p {
+    font-size: 1.2rem;
+    color: #666;
+    margin-bottom: 2rem;
+  }
+
+  a.cta {
+    display: inline-block;
+    background-color: #007bff;
+    color: #fff;
+    text-decoration: none;
+    padding: 1rem 2rem;
     border-radius: 4px;
-    cursor: pointer;
-    font-size: 0.9rem;
+    font-size: 1.2rem;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: #0056b3;
+    }
   }
 `;
 
-const ListItem = styled.li`
-  margin-bottom: 0.5rem;
-  color: #555;
+const InfoSection = styled.section`
+  margin-bottom: 3rem;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 1.5rem;
+  }
+
+  p {
+    font-size: 1.2rem;
+    color: #666;
+  }
 `;
 
 const Footer = styled.footer`
@@ -61,115 +79,50 @@ const Footer = styled.footer`
   padding-top: 2rem;
 `;
 
-const SearchPage = () => {
-  const [tagList, setTagList] = useState([]);
-  const [languageList, setLanguageList] = useState([]);
-  const [contentData, setContentData] = useState({ content: '', language: {}, tags: [] });
-  const [tagSearchText, setTagSearchText] = useState('');
-  const [languageSearchText, setLanguageSearchText] = useState('');
-  const [contentSearchText, setContentSearchText] = useState('');
-
-  const handleInputChange = (setter) => (event) => {
-    setter(event.target.value);
-  };
-
-  const fetchData = useCallback(async (url, setter) => {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        setter(data);
-      } else {
-        console.error(`Error: ${response.status} - ${response.statusText}`);
-      }
-    } catch (error) {
-      console.error(`Error fetching data:`, error);
-    }
-  }, []);
-
-  const handleFetchTagList = useCallback(() => {
-    fetchData(`${API_URL}/api/v1/text/findTextSortedByTag/${tagSearchText}`, setTagList);
-  }, [fetchData, tagSearchText]);
-
-  const handleFetchLanguageList = useCallback(() => {
-    fetchData(`${API_URL}/api/v1/text/findTextSortedByLanguage/${languageSearchText}`, setLanguageList);
-  }, [fetchData, languageSearchText]);
-
-  const handleFetchContentList = useCallback(() => {
-    fetchData(`${API_URL}/api/v1/text/getTextByContent/${contentSearchText}`, setContentData);
-  }, [fetchData, contentSearchText]);
-
-  const handleKeyPress = (event, fetchFunction) => {
-    if (event.key === 'Enter') {
-      fetchFunction();
-    }
-  };
-
+const HomePage = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Paper elevation={3} style={{ padding: '50px 20px', width: 600, margin: '20px auto' }}>
-          <Title>Text List Sorted by Tag</Title>
-          <InputGroup>
-            <input
-              type="text"
-              value={tagSearchText}
-              onChange={handleInputChange(setTagSearchText)}
-              onKeyPress={(event) => handleKeyPress(event, handleFetchTagList)}
-              placeholder="Search by tag..."
-            />
-            <button onClick={handleFetchTagList}>Find</button>
-          </InputGroup>
-          <ul>
-            {tagList.map((content, index) => (
-              <ListItem key={index}>{content}</ListItem>
-            ))}
-          </ul>
-        </Paper>
+    <Container>
+      <Header>
+        <h1>Определитель языка текста</h1>
+      </Header>
 
-        <Paper elevation={3} style={{ padding: '50px 20px', width: 600, margin: '20px auto' }}>
-          <Title>Text List Sorted by Language</Title>
-          <InputGroup>
-            <input
-              type="text"
-              value={languageSearchText}
-              onChange={handleInputChange(setLanguageSearchText)}
-              onKeyPress={(event) => handleKeyPress(event, handleFetchLanguageList)}
-              placeholder="Search by language..."
-            />
-            <button onClick={handleFetchLanguageList}>Find</button>
-          </InputGroup>
-          <ul>
-            {languageList.map((content, index) => (
-              <ListItem key={index}>{content}</ListItem>
-            ))}
-          </ul>
-        </Paper>
+      <main>
+        <HeroSection>
+          <h2>Определите язык вашего текста</h2>
+          <p>
+            Наш инструмент позволяет быстро и точно определить язык любого
+            текста. Просто введите свой текст и нажмите "Определить язык".
+          </p>
+          <a href="/search" className="cta">
+            Определить язык
+          </a>
+        </HeroSection>
 
-        <Paper elevation={3} style={{ padding: '50px 20px', width: 600, margin: '20px auto' }}>
-          <Title>Text List by Content</Title>
-          <InputGroup>
-            <input
-              type="text"
-              value={contentSearchText}
-              onChange={handleInputChange(setContentSearchText)}
-              onKeyPress={(event) => handleKeyPress(event, handleFetchContentList)}
-              placeholder="Search by content..."
-            />
-            <button onClick={handleFetchContentList}>Find</button>
-          </InputGroup>
-          <ul>
-            <ListItem>Content: {contentData.content}</ListItem>
-            <ListItem>Language: {contentData.language?.name || ''}</ListItem>
-            <ListItem>Tags: {contentData.tags.length > 0 ? contentData.tags.map((tag) => tag.name).join(', ') : ''}</ListItem>
-          </ul>
-        </Paper>
-        <Footer>
-          <p>&copy; 2024 Определитель языка текста. Все права защищены.</p>
-        </Footer>
-      </Container>
-    </ThemeProvider>
+        <InfoSection>
+          <h2>Как это работает</h2>
+          <p>
+            Наш определитель языка использует продвинутые алгоритмы машинного
+            обучения, чтобы анализировать синтаксис, словарный запас и другие
+            особенности текста и определять его язык с высокой точностью.
+          </p>
+        </InfoSection>
+
+        <InfoSection>
+          <h2>Почему это важно?</h2>
+          <p>
+            Определение языка текста важно для многих задач, таких как перевод,
+            анализ данных, управление контентом и многое другое. Наш инструмент
+            поможет вам экономить время и деньги, предоставляя точную
+            информацию о языке вашего текста.
+          </p>
+        </InfoSection>
+      </main>
+
+      <Footer>
+        <p>&copy; 2024 Определитель языка текста. Все права защищены.</p>
+      </Footer>
+    </Container>
   );
 };
 
-export default SearchPage;
+export default HomePage;
